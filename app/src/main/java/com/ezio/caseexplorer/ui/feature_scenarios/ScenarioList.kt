d2be3d00091196788e5b144cfab5c058e5fb6066
elelan's mac <elelanv@gmail.com>
2022-02-04 09:37:56 +0530

    - color and theme changed - added nav safe args

--- app/src/main/java/com/ezio/caseexplorer/ui/feature_scenarios/ScenarioList.kt
+++ app/src/main/java/com/ezio/caseexplorer/ui/feature_scenarios/ScenarioList.kt
@@ -5,17 +5,17 @@ import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
-import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.ezio.caseexplorer.R
 import com.ezio.caseexplorer.core.domain.models.ScenarioItem
 import com.ezio.caseexplorer.core.utils.exhaustive
 import com.ezio.caseexplorer.databinding.FragmentScenarioListBinding
-import dagger.hilt.android.AndroidEntryPoint;
+import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.collect
 
 @AndroidEntryPoint
-class ScenarioList: Fragment(R.layout.fragment_scenario_list), ScenarioAdapter.ItemClickListener {
+class ScenarioList : Fragment(R.layout.fragment_scenario_list), ScenarioAdapter.ItemClickListener {
 
     lateinit var binding: FragmentScenarioListBinding
     lateinit var dataAdapter: ScenarioAdapter
@@ -32,6 +32,12 @@ class ScenarioList: Fragment(R.layout.fragment_scenario_list), ScenarioAdapter.I
                 adapter = dataAdapter
                 layoutManager = LinearLayoutManager(requireContext())
                 setHasFixedSize(true)
+                addItemDecoration(
+                    DividerItemDecoration(
+                        this.context,
+                        DividerItemDecoration.VERTICAL
+                    )
+                )
             }
         }
 
@@ -60,6 +66,7 @@ class ScenarioList: Fragment(R.layout.fragment_scenario_list), ScenarioAdapter.I
                     }
                     is UiStateEvents.NavigateToCase -> {
                         // Navigate to Case Details Screen
+                        val action = ScenarioListDirections.actionScenarioListToCaseFragment()
                     }
                 }.exhaustive
             }
@@ -68,9 +75,9 @@ class ScenarioList: Fragment(R.layout.fragment_scenario_list), ScenarioAdapter.I
 
     private fun showHideLoading(isLoading: Boolean) {
         binding.apply {
-            if(isLoading) {
+            if (isLoading) {
                 progress.visibility = View.VISIBLE
-            } else{
+            } else {
                 progress.visibility = View.GONE
             }
         }
